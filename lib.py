334042e1e5bafb6b3a5a9fe4c94aa99eebba3a8a
Abhai Kollara <abhai.dilip@gmail.com>
2021-08-23 13:59:48 +0530

    Minor changes

--- lib.py
+++ lib.py
@@ -12,15 +12,6 @@ class Customer:
         self.name = name
         self.location = location
 
-class StartLocation:
-    '''
-        Wrapper class for start location
-        which is neither a restaurant or a customer
-    '''
-
-    def __init__(self, location: Location) -> None:
-        self.location = location
-
 class Order:
     def __init__(self, customer: Customer, restaurant: Restaurant, prep_time: float):
         self.customer = customer
@@ -35,28 +26,32 @@ def generate_graph(start_position: Location, orders: Iterable[Order]) -> Graph:
     g = Graph()
 
     for order in orders:
-        g.add_node(Node(id=order.customer.name, data=order.customer))
-        g.add_node(Node(id=order.restaurant.name, data=order.restaurant))
+        rn = Node(id=order.restaurant.name, data={'location':order.restaurant.location, 'type':'restaurant'})
+        cn = Node(id=order.customer.name, data={'location':order.customer.location, 'pre_req':rn, 'type':'customer'})
+        g.add_node(rn)
+        g.add_node(cn)
 
     for n1 in g.nodes.values():
         for n2 in g.nodes.values():
             if n1 is not n2:
-                print(n1.data == n2.data)
-                weight = travel_time(n1.data.location, n2.data.location, 20)
+                weight = travel_time(n1.data['location'], n2.data['location'], 20)
                 g.add_edge(n1, n2, weight, bidirectional=True)
 
-    start = Node('START', data=StartLocation(start_position))
+    start = Node('START', data={'location':start_position})
     g.add_node(start)
 
     for n in g.nodes.values():
-        if isinstance(n.data, Restaurant):
-            g.add_edge(start, n, bidirectional=True)
+        if n.data.get('type') == 'restaurant':
+            weight = travel_time(start.data['location'], n.data['location'], 20)
+            g.add_edge(start, n, weight=weight, bidirectional=True)
 
     return g
 
 # This should be implement the actual solution
 def find_optimal_delivery_path(start_position: Location, order_list: Iterable[Order]) -> Iterable[Location]:
     g = generate_graph(start_position, order_list)
+
+    # paths = generate_paths(g)
 
     # Generate all possible valid paths here and check the sum of weights ?
     # Minimum spanning tree here ?
